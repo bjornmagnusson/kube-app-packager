2f73a1fa06f41de96250f2ded6e064de3e860551
Björn Magnusson <bjornmagnusson1984@gmail.com>
2018-04-04 20:36:37 +0200

    Quote app env

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 test () {
-  echo "Testing ${1}"
+  echo "Testing $1"
   docker run --volumes-from configs $1 --env DOCKER_HOST=${DOCKER_HOST} --env DOCKER_TLS_VERIFY=${DOCKER_TLS_VERIFY} --env DOCKER_CERT_PATH=/cfg bjornmagnusson/kube-app-packager
   exit $?
 }
@@ -11,8 +11,8 @@ docker cp ${DOCKER_CERT_PATH}/ca.pem configs:/cfg
 docker cp ${DOCKER_CERT_PATH}/cert.pem configs:/cfg
 docker cp ${DOCKER_CERT_PATH}/key.pem configs:/cfg
 
-APP_ENV="--env DOCKER_IMAGES=mariadb:10.1.31,prom/mysqld-exporter:v0.10.0 --env HELM_CHART_REPOSITORY=stable --env HELM_CHART_NAME=mariadb --env HELM_CHART_VERSION=2.1.17 --env APP_VERSION=0.0.1-SNAPSHOT --env APP_NAME=mariadb"
-EXIT_CODE=$(test $APP_ENV)
+APP_ENV="--env\" DOCKER_IMAGES=mariadb:10.1.31,prom/mysqld-exporter:v0.10.0 --env HELM_CHART_REPOSITORY=stable --env HELM_CHART_NAME=mariadb --env HELM_CHART_VERSION=2.1.17 --env APP_VERSION=0.0.1-SNAPSHOT --env APP_NAME=mariadb"
+EXIT_CODE=$(test "$APP_ENV")
 if [[ $EXIT_CODE != "0" ]]; then
   echo "Test failed for $APP_ENV"
   exit 1
