dcfbd7147e3d0273414e9c379d4a458f16bb44e3
Björn Magnusson <bjornmagnusson1984@gmail.com>
2018-04-11 21:27:10 +0200

    Support for locally test

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-CWD=${1:-.}
+CWD=${1:-$PWD}
+MODE=${2:-local}
 
-docker create -v /cfg --name configs alpine:3.7 /bin/true
-docker cp ${DOCKER_CERT_PATH}/ca.pem configs:/cfg
-docker cp ${DOCKER_CERT_PATH}/cert.pem configs:/cfg
-docker cp ${DOCKER_CERT_PATH}/key.pem configs:/cfg
+if [[ $MODE != "local" ]]; then
+  docker create -v /cfg --name configs alpine:3.7 /bin/true
+  docker cp ${DOCKER_CERT_PATH}/ca.pem configs:/cfg
+  docker cp ${DOCKER_CERT_PATH}/cert.pem configs:/cfg
+  docker cp ${DOCKER_CERT_PATH}/key.pem configs:/cfg
+else
+  docker ps --filter=reference=docker-test
+  if [[ $? != "0" ]]; then
+    docker run --privileged --name docker-test -d docker:stable-dind
+  fi
+fi
 
 TEST_FILES=$CWD/test/*
 for test_file in $TEST_FILES; do
@@ -12,6 +20,7 @@ for test_file in $TEST_FILES; do
 done
 
 for test_file in $TEST_FILES; do
+  cd $CWD
   if [ -d "$test_file" ]; then
     echo "$test_file is a directory, skipping"
     continue
@@ -32,15 +41,25 @@ for test_file in $TEST_FILES; do
     docker cp $CWD/test/test_single_with_scripts_install.sh app:/app
   fi
   APP_PACKAGE_CONTAINER=$(basename $test_file)
-  docker run \
-    --volumes-from configs \
-    $APP_ENV \
-    --env DOCKER_HOST=${DOCKER_HOST} \
-    --env DOCKER_TLS_VERIFY=${DOCKER_TLS_VERIFY} \
-    --env DOCKER_CERT_PATH=/cfg \
-    --volumes-from app \
-    --name $APP_PACKAGE_CONTAINER \
-  bjornmagnusson/kube-app-packager
+  if [[ $MODE == "local" ]]; then
+    DIND_HOST=$(docker inspect docker-test --format='{{ .NetworkSettings.Networks.bridge.IPAddress }}')
+    docker run \
+      $APP_ENV \
+      --volumes-from app \
+      --env DOCKER_HOST=${DIND_HOST} \
+      --name $APP_PACKAGE_CONTAINER \
+    bjornmagnusson/kube-app-packager
+  else
+    docker run \
+      --volumes-from configs \
+      $APP_ENV \
+      --env DOCKER_HOST=${DOCKER_HOST} \
+      --env DOCKER_TLS_VERIFY=${DOCKER_TLS_VERIFY} \
+      --env DOCKER_CERT_PATH=/cfg \
+      --volumes-from app \
+      --name $APP_PACKAGE_CONTAINER \
+    bjornmagnusson/kube-app-packager
+  fi
 
   if [[ $? != "0" ]]; then
     echo "Test failed for $test_file"
@@ -62,7 +81,7 @@ for test_file in $TEST_FILES; do
   tar tf $CWD/$APP_PACKAGE
   mkdir $CWD/$APP_PACKAGE_CONTAINER
   echo "Unpacking package into $CWD/$APP_PACKAGE_CONTAINER"
-  tar zxvf $1/$APP_PACKAGE -C $CWD/$APP_PACKAGE_CONTAINER
+  tar zxvf $CWD/$APP_PACKAGE -C $CWD/$APP_PACKAGE_CONTAINER
   cd $CWD/$APP_PACKAGE_CONTAINER
 
   SCRIPTS=$(docker inspect --format="{{range .Config.Env}}{{println .}}{{end}}" $APP_PACKAGE_CONTAINER | grep SCRIPTS | cut -d= -f2)
@@ -83,13 +102,27 @@ for test_file in $TEST_FILES; do
     DOCKER_IMAGES_ARR=$(echo "$DOCKER_IMAGES" | sed "s/,/ /g")
     for DOCKER_IMAGE in $DOCKER_IMAGES_ARR; do
       echo "Deleting Docker image $DOCKER_IMAGE"
-      docker rmi -f $DOCKER_IMAGE
+      if [[ $MODE == "local" ]]; then
+        docker run --rm --env DOCKER_HOST=$DIND_HOST docker:stable rmi -f $DOCKER_IMAGE
+      else
+        docker rmi -f $DOCKER_IMAGE
+      fi
     done
   fi
-  docker load --input images.tar
+
+  if [[ $MODE == "local" ]]; then
+    docker run --rm --env DOCKER_HOST=$DIND_HOST -v $PWD:/images docker:stable load --input /images/images.tar
+  else
+    docker load --input images.tar
+  fi
   for UNTAG_REPOSITORY in $UNTAG_REPOSITORIES_ARR; do
     echo "Validating repository $UNTAG_REPOSITORY has been untagged"
-    docker images | grep $UNTAG_REPOSITORY
+    if [[ $MODE == "local" ]]; then
+      docker run --rm --env DOCKER_HOST=$DIND_HOST docker:stable images | grep $UNTAG_REPOSITORY
+    else
+      docker images | grep $UNTAG_REPOSITORY
+    fi
+
     if [[ $? != "1" ]]; then
       echo "Test failed for $test_file, failed to untag $UNTAG_REPOSITORY images"
       exit 1
@@ -98,5 +131,6 @@ for test_file in $TEST_FILES; do
 
   echo "Cleaning up"
   rm -rf $CWD/$APP_PACKAGE $CWD/$APP_PACKAGE_CONTAINER
+  docker rm $APP_PACKAGE_CONTAINER
   docker rm app
 done
